69027365e23209d0b57450f9cd4723c3c57e52d9
tuananh <letuananh2663@gmail.com>
2024-10-22 17:02:32 +0700

    MainActivity

--- app/src/main/java/com/foodapp/Activity/MainActivity.java
+++ app/src/main/java/com/foodapp/Activity/MainActivity.java
@@ -1,19 +1,142 @@
 package com.foodapp.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.foodapp.Adapter.BestFoodsAdapter;
+import com.foodapp.Domain.Foods;
+import com.foodapp.Domain.Location;
+import com.foodapp.Domain.Price;
+import com.foodapp.Domain.Time;
 import com.foodapp.R;
+import com.foodapp.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends BaseActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initLocation();
+        initTime();
+        initPrice();
+        initBestFood();
+    }
+
+    private void initBestFood() {
+        DatabaseReference myRef = database.getReference("Foods");
+        binding.progressBarBestFood.setVisibility(View.VISIBLE);
+        ArrayList<Foods> list = new ArrayList<>();
+        Query query = myRef.orderByChild("BestFood").equalTo(true);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot issue: dataSnapshot.getChildren()){
+                        list.add(issue.getValue(Foods.class));
+                    }
+                    if(list.size()>0){
+                        binding.bestFoodView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false));
+                        RecyclerView.Adapter adapter = new BestFoodsAdapter(list);
+                        binding.bestFoodView.setAdapter(adapter);
+                    }
+                    binding.progressBarBestFood.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void initLocation() {
+        DatabaseReference myRef = database.getReference("Location");
+        ArrayList<Location> list = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot issue: dataSnapshot.getChildren()){
+                        list.add(issue.getValue(Location.class));
+                    }
+                    ArrayAdapter<Location> adapter = new ArrayAdapter<>(MainActivity.this,R.layout.sp_item,list);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    binding.locationSp.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void initTime() {
+        DatabaseReference myRef = database.getReference("Time");
+        ArrayList<Time> list = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot issue: dataSnapshot.getChildren()){
+                        list.add(issue.getValue(Time.class));
+                    }
+                    ArrayAdapter<Time> adapter = new ArrayAdapter<>(MainActivity.this,R.layout.sp_item,list);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    binding.timeSp.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void initPrice() {
+        DatabaseReference myRef = database.getReference("Price");
+        ArrayList<Price> list = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot issue: dataSnapshot.getChildren()){
+                        list.add(issue.getValue(Price.class));
+                    }
+                    ArrayAdapter<Price> adapter = new ArrayAdapter<>(MainActivity.this,R.layout.sp_item,list);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    binding.priceSp.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
